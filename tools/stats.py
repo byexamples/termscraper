ac7c72df2957fc323e3c1d39423122a9824f96f6
Martin Di Paola <martinp.dipaola@gmail.com>
2022-08-15 16:00:59 -0300

    chore: stats.py to pretty print stats about screen/stream

---
+++ tools/stats.py
@@ -0,0 +1,37 @@
+import io
+import os.path
+import sys
+
+import termscraper
+
+
+if __name__ == "__main__":
+    benchmark = os.environ["BENCHMARK"]
+    lines, columns = map(int, os.environ.get("GEOMETRY", "24x80").split('x'))
+    optimize_conf = int(os.environ.get("OPTIMIZECONF", "0"))
+    sys.argv.extend(["--inherit-environ", "BENCHMARK,GEOMETRY,OPTIMIZECONF"])
+
+    with io.open(benchmark, "rb") as handle:
+        data = handle.read()
+
+    extra_args = {}
+    if optimize_conf:
+        extra_args = {
+                'track_dirty_lines': False,
+                'styleless': True,
+                }
+
+    screen = termscraper.Screen(columns, lines, **extra_args)
+    stream = termscraper.ByteStream(screen, trace_callbacks=True)
+
+    stream.feed(data)
+
+    print("Terminal input:", os.path.basename(benchmark))
+    print("Stream stats:")
+    print(stream.stats())
+    print()
+
+    print("Screen stats:")
+    print(screen.stats())
+
+
